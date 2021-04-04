6601acc0e8fb83b77a5275fb5fcc585d0f53c486
dimarionss <dimarionss@gmail.com>
2021-04-04 18:52:02 +0300

    добавил динамику

--- js/app.js
+++ js/app.js
@@ -45,20 +45,20 @@ new Vue({
     ],
     icons_add_visitka: [],
     icon_dialog: [
-      {text_icon:'mdi-telegram', color_icon:'background: #249bd7;'},
-      {text_icon:'mdi-facebook', color_icon:'background: #3b5998;'},
-      {text_icon:'mdi-instagram', color_icon:'background: #3f729b;'},
-      {text_icon:'mdi-twitter', color_icon:'background: #00acee;'},
-      {text_icon:'mdi-google', color_icon:'background: #DD4B39;'},
-      {text_icon:'mdi-twitch', color_icon:'background: #591381;'},
-      {text_icon:'mdi-pinterest', color_icon:'background: #c8232c;'},
-      {text_icon:'mdi-whatsapp', color_icon:'background: #50b154;'},
-      {text_icon:'mdi-skype', color_icon:'background: #00aff0;'},
-      {text_icon:'mdi-youtube', color_icon:'background: #c4302b;'},
-      {text_icon:'mdi-vk', color_icon:'background: #5d84ae;'},
-      {text_icon:'mdi-odnoklassniki', color_icon:'background: #f93;'},
-      {text_icon:'mdi-linkedin', color_icon:'background: #0e76a8;'},
-      {text_icon:'fab fa-viber', color_icon:'background: #665CAC;'},
+      {text_icon:'mdi-telegram', color_icon:'background: #249bd7;', name_icon: 'Telegram'},
+      {text_icon:'mdi-facebook', color_icon:'background: #3b5998;', name_icon: 'Facebook'},
+      {text_icon:'mdi-instagram', color_icon:'background: #3f729b;', name_icon: 'Instagram'},
+      {text_icon:'mdi-twitter', color_icon:'background: #00acee;', name_icon: 'Twitter'},
+      {text_icon:'mdi-google', color_icon:'background: #DD4B39;', name_icon: 'Google'},
+      {text_icon:'mdi-twitch', color_icon:'background: #591381;', name_icon: 'Twitch'},
+      {text_icon:'mdi-pinterest', color_icon:'background: #c8232c;', name_icon: 'Pinterest'},
+      {text_icon:'mdi-whatsapp', color_icon:'background: #50b154;', name_icon: 'WhatsApp'},
+      {text_icon:'mdi-skype', color_icon:'background: #00aff0;', name_icon: 'Skype'},
+      {text_icon:'mdi-youtube', color_icon:'background: #c4302b;', name_icon: 'YouTube'},
+      {text_icon:'mdi-vk', color_icon:'background: #5d84ae;', name_icon: 'Vkontakte'},
+      {text_icon:'mdi-odnoklassniki', color_icon:'background: #f93;', name_icon: 'Odnoklassniki'},
+      {text_icon:'mdi-linkedin', color_icon:'background: #0e76a8;', name_icon: 'LinkedIn'},
+      {text_icon:'fab fa-viber', color_icon:'background: #665CAC;', name_icon: 'Viber'},
     ],
     readlink : false,
     toggle: false,
@@ -86,8 +86,8 @@ new Vue({
     resetLink(){
       this.readlink = false
     },
-    add_form_icon(text_icon, color_icon){
-        this.icons_add_visitka.push({text_icon:text_icon, color_icon:color_icon})
+    add_form_icon(text_icon, color_icon, name_icon){
+        this.icons_add_visitka.push({text_icon:text_icon, color_icon:color_icon, name_icon: name_icon})
     }
   },
   computed: {
