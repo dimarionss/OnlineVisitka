2ebb44108367c913d8324997781405a5bf8d8fc6
Dmitriy Roi <dimarionss@gmail.com>
2021-04-12 14:47:20 +0300

    изменения

--- js/app.js
+++ js/app.js
@@ -64,110 +64,191 @@ new Vue({
       'mdi-telegram',
       'mdi-instagram',
     ],
-    linkIconForm:{},
+    linkIconForm: {},
     icons_add_visitka: [{
-      text_icon: 'mdi-telegram',
-      color_icon: 'background: #249bd7;',
-      name_icon: 'Telegram',
-      link_social: 'https://t.me/'
-    },
-    {
-      text_icon: 'mdi-facebook',
-      color_icon: 'background: #3b5998;',
-      name_icon: 'Facebook',
-      link_social: 'https://www.facebook.com/'
-    },
-    {
-      text_icon: 'mdi-instagram',
-      color_icon: 'background: #3f729b;',
-      name_icon: 'Instagram',
-      link_social: 'https://instagram.com/'
-    }],
-    icon_dialog: [{
         text_icon: 'mdi-telegram',
+        icon_img_link: '',
         color_icon: 'background: #249bd7;',
         name_icon: 'Telegram',
-        link_social: 'https://t.me/'
+        link_social: 'https://t.me/',
+        link_description: 'Ник/Логин',
       },
       {
         text_icon: 'mdi-facebook',
+        icon_img_link: '',
         color_icon: 'background: #3b5998;',
         name_icon: 'Facebook',
-        link_social: 'https://www.facebook.com/'
+        link_social: 'https://www.facebook.com/',
+        link_description: 'Ник/Логин',
       },
       {
         text_icon: 'mdi-instagram',
+        icon_img_link: './img/icon/instagram.png',
         color_icon: 'background: #3f729b;',
         name_icon: 'Instagram',
-        link_social: 'https://instagram.com/'
+        link_social: 'https://instagram.com/',
+        link_description: 'Ник/Логин',
+      },
+    ],
+
+
+
+
+
+    // главный массив соц. иконок =======================================
+    icon_dialog: [{
+        text_icon: 'mdi-telegram',
+        icon_img_link: '',
+        color_icon: 'background: #249bd7;',
+        name_icon: 'Telegram',
+        link_social: 'https://t.me/',
+        link_description: 'Ник/Логин',
+      },
+      {
+        text_icon: 'mdi-facebook',
+        icon_img_link: '',
+        color_icon: 'background: #3b5998;',
+        name_icon: 'Facebook',
+        link_social: 'https://www.facebook.com/',
+        link_description: 'Ник/Логин',
+      },
+      {
+        text_icon: 'mdi-instagram',
+        icon_img_link: './img/icon/instagram.png',
+        color_icon: 'background: #3f729b;',
+        name_icon: 'Instagram',
+        link_social: 'https://instagram.com/',
+        link_description: 'Ник/Логин',
       },
       {
         text_icon: 'mdi-twitter',
+        icon_img_link: '',
         color_icon: 'background: #00acee;',
         name_icon: 'Twitter',
-        link_social: 'https://twitter.com/'
+        link_social: 'https://twitter.com/',
+        link_description: 'Ник/Логин',
       },
       {
         text_icon: 'mdi-google',
+        icon_img_link: '',
         color_icon: 'background: #DD4B39;',
         name_icon: 'Google',
-        link_social: 'https://plus.google.com/'
+        link_social: 'https://plus.google.com/',
+        link_description: 'Ник/Логин',
       },
       {
         text_icon: 'mdi-twitch',
+        icon_img_link: '',
         color_icon: 'background: #591381;',
         name_icon: 'Twitch',
-        link_social: 'https://www.twitch.tv/'
+        link_social: 'https://www.twitch.tv/',
+        link_description: 'Ник/Логин',
       },
       {
         text_icon: 'mdi-pinterest',
+        icon_img_link: '',
         color_icon: 'background: #c8232c;',
         name_icon: 'Pinterest',
-        link_social: 'https://www.pinterest.ru/'
+        link_social: 'https://www.pinterest.ru/',
+        link_description: 'Ник/Логин',
       },
       {
         text_icon: 'mdi-whatsapp',
+        icon_img_link: '',
         color_icon: 'background: #50b154;',
         name_icon: 'WhatsApp',
-        link_social: 'https://api.whatsapp.com/send?phone='
+        link_social: 'https://api.whatsapp.com/send?phone=',
+        link_description: 'Номер телефона',
       },
       {
         text_icon: 'mdi-skype',
+        icon_img_link: '',
         color_icon: 'background: #00aff0;',
         name_icon: 'Skype',
-        link_social: 'skype:'
+        link_social: 'skype:',
+        link_description: 'Ник/Логин',
       },
       {
         text_icon: 'mdi-youtube',
+        icon_img_link: '',
         color_icon: 'background: #c4302b;',
         name_icon: 'YouTube',
-        link_social: ''
+        link_social: '',
+        link_description: 'Ссылка на канал',
       },
       {
         text_icon: 'mdi-vk',
+        icon_img_link: '',
         color_icon: 'background: #5d84ae;',
         name_icon: 'Vkontakte',
-        link_social: 'https://vk.com/'
+        link_social: 'https://vk.com/',
+        link_description: 'Ник/Логин',
       },
       {
         text_icon: 'mdi-odnoklassniki',
+        icon_img_link: '',
         color_icon: 'background: #f93;',
         name_icon: 'Odnoklassniki',
-        link_social: 'https://ok.ru/profile/'
+        link_social: 'https://ok.ru/profile/',
+        link_description: 'Ник/Логин',
       },
       {
         text_icon: 'mdi-linkedin',
+        icon_img_link: '',
         color_icon: 'background: #0e76a8;',
         name_icon: 'LinkedIn',
-        link_social: 'https://www.linkedin.com/in/'
+        link_social: 'https://www.linkedin.com/in/',
+        link_description: 'Ник/Логин',
       },
       {
         text_icon: 'fab fa-viber',
+        icon_img_link: '',
         color_icon: 'background: #665CAC;',
         name_icon: 'Viber',
-        link_social: 'https://viber.com/send?phone='
+        link_social: 'https://viber.com/send?phone=',
+        link_description: 'Номер телефона',
       },
     ],
+    // главный массив соц. иконок =======================================
+    // главный массив контактных иконок =======================================
+    icon_call_dialog: [{
+        text_icon: 'mdi-map',
+        icon_img_link: '',
+        color_icon: 'background: #50b154;',
+        name_icon: 'Адрес',
+        link_social: 'https://www.google.com/maps/search/?api=1&query=',
+        link_description: 'Адресс фирмы',
+      },
+      {
+        text_icon: 'mdi-phone',
+        icon_img_link: '',
+        color_icon: 'background: #50b154;',
+        name_icon: 'Позвонить',
+        link_social: 'tel:',
+        link_description: 'Номер телефона',
+      },
+      {
+        text_icon: 'mdi-mail',
+        icon_img_link: '',
+        color_icon: 'background: #50b154;',
+        name_icon: 'Почта',
+        link_social: 'mailto:',
+        link_description: 'Почтовый адрес',
+      },
+      {
+        text_icon: 'mdi-message',
+        icon_img_link: '',
+        color_icon: 'background: #50b154;',
+        name_icon: 'Сообщение',
+        link_social: 'sms:',
+        link_description: 'Номер телефона',
+      },
+    ],
+
+    // главный массив контактных иконок =======================================
+
+
+
 
     readlink: false,
     toggle: false,
@@ -177,25 +258,25 @@ new Vue({
     // link social end==============================
 
     show: false,
-    
-      // =======================cropp======================== 
-      cropper: null,
-      cropperava: null,
-      objectUrl: null,
-      objectUrlava: null,
-      previewCropped: null,
-      previewCroppedava: null,
-      selectedFile: null,
-      selectedFileava: null,
-      // =======================cropp======================== 
+
+    // =======================cropp======================== 
+    cropper: null,
+    cropperava: null,
+    objectUrl: null,
+    objectUrlava: null,
+    previewCropped: null,
+    previewCroppedava: null,
+    selectedFile: null,
+    selectedFileava: null,
+    // =======================cropp======================== 
   }),
-  
+
   methods: {
     validate() {
       window.location.href = 'login.html'
       this.$refs.form.validate()
     },
-    location(){
+    location() {
       window.location.href = 'index.html'
     },
     reset() {
@@ -204,30 +285,32 @@ new Vue({
     resetValidation() {
       this.$refs.form.resetValidation()
     },
-    
+
     resetLink() {
       this.readlink = false
       this.linkIcon = 'mdi-lock-open-variant'
     },
-    deleteIcon(index){
+    deleteIcon(index) {
       this.icons_add_visitka.splice(index, 1)
     },
-    addLinkIcon(){
+    addLinkIcon() {
       this.icons_add_visitka.forEach((item, index) => {
         this.$set(item, 'link_icon', this.linkIconForm[index])
         console.log(this.icons_add_visitka)
       })
       // this.linkIconForm = {}
     },
-    linkLocation(linkSocial, iconLink){
-      window.location = linkSocial+iconLink;
+    linkLocation(linkSocial, iconLink) {
+      window.location = linkSocial + iconLink;
     },
-    add_form_icon(text_icon, color_icon, name_icon, link_social) {
+    add_form_icon(text_icon, icon_img, color_icon, name_icon, link_social, link_description) {
       this.icons_add_visitka.push({
         text_icon: text_icon,
+        icon_img_link: icon_img,
         color_icon: color_icon,
         name_icon: name_icon,
-        link_social: link_social
+        link_social: link_social,
+        link_description: link_description,
       })
       this.dialog = false
     },
@@ -237,49 +320,52 @@ new Vue({
 
 
     // =======================cropp======================== 
-    change({coordinates, canvas}) {
+    change({
+      coordinates,
+      canvas
+    }) {
       console.log(coordinates, canvas)
-   },
+    },
 
 
-   setupCropper (selectedFile) {
-    if (this.cropper) {
-      this.cropper.destroy()
-    }
+    setupCropper(selectedFile) {
+      if (this.cropper) {
+        this.cropper.destroy()
+      }
 
-    if (this.objectUrl) {
-      window.URL.revokeObjectURL(this.objectUrl)
-    }
+      if (this.objectUrl) {
+        window.URL.revokeObjectURL(this.objectUrl)
+      }
 
-    if (!selectedFile) {
-      this.cropper = null
-      this.objectUrl = null
-      this.previewCropped = null
-      return
-    }
+      if (!selectedFile) {
+        this.cropper = null
+        this.objectUrl = null
+        this.previewCropped = null
+        return
+      }
 
-    this.objectUrl = window.URL.createObjectURL(selectedFile)
-    this.$nextTick(this.setupCropperInstance)
-  },
-  setupCropperava (selectedFileava) {
-    if (this.cropperava) {
-      this.cropperava.destroy()
-    }
+      this.objectUrl = window.URL.createObjectURL(selectedFile)
+      this.$nextTick(this.setupCropperInstance)
+    },
+    setupCropperava(selectedFileava) {
+      if (this.cropperava) {
+        this.cropperava.destroy()
+      }
 
-    if (this.objectUrlava) {
-      window.URL.revokeObjectURL(this.objectUrlava)
-    }
+      if (this.objectUrlava) {
+        window.URL.revokeObjectURL(this.objectUrlava)
+      }
 
-    if (!selectedFileava) {
-      this.cropperava = null
-      this.objectUrlava = null
-      this.previewCroppedava = null
-      return
-    }
+      if (!selectedFileava) {
+        this.cropperava = null
+        this.objectUrlava = null
+        this.previewCroppedava = null
+        return
+      }
 
-    this.objectUrlava = window.URL.createObjectURL(selectedFileava)
-    this.$nextTick(this.setupCropperInstanceava)
-  },
+      this.objectUrlava = window.URL.createObjectURL(selectedFileava)
+      this.$nextTick(this.setupCropperInstanceava)
+    },
     // =======================cropp======================== 
   },
   computed: {
@@ -290,12 +376,12 @@ new Vue({
 
       return 'Аккаунт вкл.'
     },
-    linkIcon: function() {
+    linkIcon: function () {
       if (this.readlink) {
         return 'mdi-lock'
-      } 
-      return 'mdi-lock-open-variant'    
+      }
+      return 'mdi-lock-open-variant'
     },
   }
-  
+
 })
